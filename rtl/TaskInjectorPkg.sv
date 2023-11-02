c2a7c6245babc4f1a87d1202446d38672a4e5a78
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2023-11-02 15:38:43 -0300

    Add Injector package

---
+++ rtl/TaskInjectorPkg.sv
@@ -0,0 +1,13 @@
+package TaskInjectorPkg;
+
+    parameter HEADER_SIZE = 13;
+
+    parameter logic [31:0] MESSAGE_REQUEST        = 32'h00000000;
+    parameter logic [31:0] MESSAGE_DELIVERY       = 32'h00000001;
+    parameter logic [31:0] NEW_APP                = 32'h00000010;
+    parameter logic [31:0] APP_ALLOCATION_REQUEST = 32'h00000026;
+    parameter logic [31:0] DATA_AV                = 32'h00000031;
+    parameter logic [31:0] APP_MAPPING_COMPLETE   = 32'h00000034;
+    parameter logic [31:0] TASK_ALLOCATION        = 32'h00000040;
+
+endpackage
